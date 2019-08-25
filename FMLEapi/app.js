e886945e821bc31c1630291ca242f0117eb72b96
Brandon <brandonlaraerazo@gmail.com>
2019-08-25 17:50:19 +0200

    Se ha eliminado todo
    Se ha borrado todo porque no se puede crear una app react con mas carpetas

--- FMLEapi/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var events = require('./eventosAPI/eventos');
-var mqtt = require('./mqttAPI/mqtt');
-app.use('/fullcalendar', events);
-app.use('/mqtt',mqtt);
-app.listen(8080);
-
