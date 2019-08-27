30e6a3a80a4d23de4a22e8ff65dad27a903c90ad
Brandon <brandonlaraerazo@gmail.com>
2019-08-28 00:50:52 +0200

    Migracion a react automatica

---
+++ precache-manifest.78570becd9619212809375c2d24c7f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c4faef927f18a0941fbefe4bb2240c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd562f8cc724b6c5894",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b1ff6d194643084e9c3e",
+    "url": "/static/js/2.c8d80cab.chunk.js"
+  },
+  {
+    "revision": "bcd562f8cc724b6c5894",
+    "url": "/static/js/main.81601117.chunk.js"
+  },
+  {
+    "revision": "2922c6b2515453cb88f1",
+    "url": "/static/js/runtime~main.91594c4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
